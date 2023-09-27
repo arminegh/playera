a0551e49e70ea8e72c22fd5c8d8334ff3f95c421
Armine Garcia <armineg@hotmail.com>
2023-09-26 19:15:26 -0600

    Pedido terminado

--- js/script.js
+++ js/script.js
@@ -1,50 +1,64 @@
 
 let opcion = prompt('Elige una opción: \nA- Agregar playera \nT - Terminar');
 
+
 while (opcion != 't' && opcion != 'T') {
 
-    let tipoCara = prompt('Elige una opción: \n01- Ovalada \n02- Redonda \n03- Alargada \nT - Cancelar');
-    if ( tipoCara == "t" && tipoCara == "T" ) {
+
+    let tipoCara = prompt('Elige el tipo de cara: \n01- Ovalada \n02- Redonda \n03- Alargada \nT - Cancelar');
+   
+    if ( tipoCara == 't' || tipoCara == 'T' ) {
         alert('Cancelando tu pedido.')
         break;
     }
 
-    let piel = prompt('Elige una opción: \n01- Blanca \n02- Moreno \n03- Medio \nT - Cancelar');
-    if ( piel == "t" && piel == "T" ) {
+    let piel = prompt('Elige el color de piel: \n01- Blanca \n02- Moreno \n03- Medio \nT - Cancelar');
+    if ( piel == "t" || piel == "T" ) {
         alert('Cancelando tu pedido.')
         break;
     }
-    let ojos = prompt('Elige una opción: \n01- Grandes \n02- Redondos \n03- Pequeños \nT - Cancelar');
-    if ( ojos == "t" && ojos == "T" ) {
+    
+    let ojos = prompt('Elige el tipo de ojos: \n01- Grandes \n02- Redondos \n03- Pequeños \nT - Cancelar');
+    if ( ojos == "t" || ojos == "T" ) {
         alert('Cancelando tu pedido.')
         break;
     }
-    let nariz = prompt('Elige una opción: \n01- Recta \n02- Ancha \n03- Delgada \nT - Cancelar');
-    if ( nariz == "t" && nariz == "T" ) {
+    
+    let nariz = prompt('Elige el tipo de nariz: \n01- Recta \n02- Ancha \n03- Delgada \nT - Cancelar');
+    if ( nariz == "t" || nariz == "T" ) {
         alert('Cancelando tu pedido.')
         break;
     }
-    let tipoCabello = prompt('Elige una opción: \n01- Lacio \n02- Rizado \n03- Pelón \nT - Cancelar');
-    if ( tipoCabello == "t" && tipoCabello == "T" ) {
+    
+    let tipoCabello = prompt('Elige el tipo de cabello: \n01- Lacio \n02- Rizado \n03- Pelón \nT - Cancelar');
+    if ( tipoCabello == "t" || tipoCabello == "T" ) {
         alert('Cancelando tu pedido.')
         break;
     }
-    let colorCabello = prompt('Elige una opción: \n01- Obscuro \n02- Rubio \n03- Castaño \nT - Cancelar');
-    if ( colorCabello == "t" && colorCabello == "T" ) {
-        alert('Cancelando tu pedido.')
-        break;
 
-        tipoCara, piel, ojos, nariz, boca, tipoCabello, colorCabello  = 99
+    let boca = prompt('Elige el tipo de cabello: \n01- Delgada \n02- Gruesa \n03- Mediana \nT - Cancelar');
+    if ( boca == "t" || boca == "T" ) {
+        alert('Cancelando tu pedido.')
+        break;
+    }
 
-    opcion = prompt('Confirma tu pedido: \n Cara ' + tipoCara + '\nPiel ' + piel + '\Ojos ' + ojos + '\Nariz ' + nariz + '\nBoca ' + boca + '\nCabello ' + tipoCabello + ' ' + colorCabello + '\nT - Cancelar')
-    if ( colorCabello == "t" && colorCabello == "T" ){
+    let colorCabello = prompt('Elige el color de cabello: \n01- Lacio \n02- Rizado \n03- Pelón \nT - Cancelar');
+    if ( colorCabello == "t" || colorCabello == "T" ) {
+        alert('Cancelando tu pedido.')
+        break;
+    }
+
+
+    opcion = prompt('Confirma tu pedido: \n Cara ' + tipoCara + '. Piel ' + piel + '. Ojos ' + ojos + '\nNariz ' + nariz + '. Boca ' + boca + '\nCabello ' + tipoCabello + ' ' + colorCabello + '\nT - Cancelar - Cualquier otra opción para confirmar.')
+
+    if ( colorCabello != "t" && colorCabello != "T" ){
         alert('Playera agregada, enviada a producion. Espera tu envíio')
     } else {
         alert('Pedido cancelado')    
     }
 
 
-        
+    
     opcion = prompt('Elige una opción: \nA- Agregar playera \nT - Terminar');
 }
 
